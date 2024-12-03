67d1242eb7d1351b782ff4069ac935cee36492ea
Jan Skrasek <hrach.cz@gmail.com>
2024-12-03 19:30:50 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,23 @@
+import kotlin.math.absoluteValue
+
 fun main() {
 	fun part1(input: List<String>): Int {
-		return input.size
+		val inputs = input.map { line -> line.split("   ").map { it.toInt() } }
+		val first = inputs.map { it[0] }.sorted()
+		val second = inputs.map { it[1] }.sorted()
+		return first.zip(second).fold(0) { acc, pair -> acc + (pair.first - pair.second).absoluteValue }
 	}
 
 	fun part2(input: List<String>): Int {
-		return input.size
+		val inputs = input.map { line -> line.split("   ").map { it.toInt() } }
+		val first = inputs.map { it[0] }.sorted()
+		val second = inputs.map { it[1] }.sorted()
+		return first.sumOf { num -> second.count { it == num }.times(num) }
 	}
 
-	check(part1(listOf("test_input")) == 1)
-
 	val testInput = readInput("Day01_test")
-	check(part1(testInput) == 1)
+	check(part1(testInput) == 11)
+	check(part2(testInput) == 31)
 
 	val input = readInput("Day01")
 	part1(input).println()
