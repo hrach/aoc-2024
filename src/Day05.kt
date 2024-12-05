b1e0dae6ea33c9af754aecabc2d7d39510151197
Jan Skrasek <hrach.cz@gmail.com>
2024-12-05 16:43:53 +0100

    refactor day 05

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,58 +1,39 @@
-import kotlin.math.roundToInt
-
 fun main() {
-	fun parse(input: List<String>): Pair<Map<Int, List<Int>>, List<List<Int>>> {
-		val rules = input
-			.takeWhile { it.contains("|") }
-			.map { it.split('|').map { it.toInt() } }
-			.groupBy { it[1] }
-			.mapValues { it.value.map { it.first() } }
+	fun parse(input: List<String>): Pair<List<Pair<Int, Int>>, List<List<Int>>> {
+		val rules = input.takeWhile { it.contains("|") }
+			.map { it.substringAfter('|').toInt() to it.substringBefore('|').toInt() }
 		val pages = input.dropWhile { !it.contains(',') }
 			.map { it.split(',').map { it.toInt() } }
 		return rules to pages
 	}
 
-	fun isOk(pages: List<Int>, rules: Map<Int, List<Int>>): Boolean =
-		pages.indices.all { i ->
-			val num = pages[i]
-			val numRules = rules[num]
-			numRules?.all { beforeNum ->
-				pages.indexOf(beforeNum) < i // -1~not found matches the condition as well
-			} ?: true
-		}
-
-	fun createComparator(rules: Map<Int, List<Int>>): Comparator<Int> =
+	fun createComparator(rules: List<Pair<Int, Int>>): Comparator<Int> =
 		Comparator { a, b ->
-			val bBefore = rules[a]?.find { it == b } != null // b has to be before
-			val aBefore = rules[b]?.find { it == a } != null // a has to be before
+			val aBefore = rules.find { it.first == b && it.second == a } != null // a has to be before
+			val bBefore = rules.find { it.first == a && it.second == b } != null // b has to be before
 			if (bBefore) 1
 			else if (aBefore) -1
 			else 0
 		}
 
-	fun part1(input: List<String>): Long {
+	fun sums(input: List<String>): Pair<Int, Int> {
 		val (rules, pages) = parse(input)
-		return pages.sumOf {
-			val isOk = isOk(it, rules)
-			if (isOk) it[(it.size / 2f).roundToInt() - 1].toLong() else 0L
+		var (sumValid, sumInvalid) = 0 to 0
+		val comparator = createComparator(rules)
+		pages.forEach { page ->
+			val sorted = page.sortedWith(comparator)
+			if (sorted == page) sumValid += page[page.size / 2]
+			else sumInvalid += sorted[sorted.size / 2]
 		}
+		return sumValid to sumInvalid
 	}
 
-	fun part2(input: List<String>): Long {
-		val (rules, pages) = parse(input)
-		val comparator = createComparator(rules)
-		return pages.sumOf {
-			val isOk = isOk(it, rules)
-			if (!isOk) {
-				val fixed = it.sortedWith(comparator)
-				fixed[(fixed.size / 2f).roundToInt() - 1].toLong()
-			} else 0L
-		}
-	}
+	fun part1(input: List<String>): Int = sums(input).first
+	fun part2(input: List<String>): Int = sums(input).second
 
 	val testInput = readInput("Day05_test")
-	check(part1(testInput) == 143L)
-	check(part2(testInput) == 123L)
+	check(part1(testInput) == 143)
+	check(part2(testInput) == 123)
 
 	val input = readInput("Day05")
 	part1(input).println()
